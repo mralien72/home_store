ea7bbdf0ef81d3f41d2fb4afbf5f31f34a05366f
Andrey <Primer@mail.ru>
2024-09-02 17:25:54 +0500

    create about, modify def in view.py

--- app/main/views.py
+++ app/main/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {'title': 'Home Page',
-               'content': 'Главная страница магазина - HOME',
+    context = {'title': 'Home - Главная',
+               'content': 'Магазин мебели HOME',
                }
-
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('about page')
+    context = {'title': 'Home - О нас',
+               'content': 'О нас',
+               'text_on_page':  'Это заглушка текста о нас пока сайт находися на стадии подготовки. '
+                                'Это заглушка текста о нас пока сайт находися на стадии подготовки. '
+                                'Это заглушка текста о нас пока сайт находися на стадии подготовки. '
+                                'Это заглушка текста о нас пока сайт находися на стадии подготовки. '
+               }
+    return render(request, 'main/about.html', context)
