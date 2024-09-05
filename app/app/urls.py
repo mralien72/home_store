dc2394aae8f02c231a65366c58926a20cff04959
Andrey <mralien72@yandex.ru>
2024-09-05 12:54:01 +0500

    path media settings & create folder media

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog')),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
