12527e08bc3c582622c91226c8bdb100c6e5778d
Andrey <mralien72@yandex.ru>
2024-09-09 00:51:47 +0500

    registration form, def and logout def

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -10,6 +10,24 @@ class UserLoginForm(AuthenticationForm):
         model = User
         fields = ['username', 'password']
 
-    # username = forms.CharField()
-    # password = forms.CharField()
+    username = forms.CharField()
+    password = forms.CharField()
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+        )
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.EmailField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
 
--- app/users/views.py
+++ app/users/views.py
@@ -1,10 +1,10 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from users.forms import UserLoginForm
-
+from users.forms import UserRegistrationForm
 
 
 def login(request):
@@ -28,8 +28,19 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
         'title': 'Home - Регистрация',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
@@ -42,4 +53,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
